a9570950e4d0e6a9f0c4a9a62f0e224f2e2e0195
dsturf <dsturf.aws1@gmail.com>
2023-05-22 21:09:59 +0900

    html5 web cam

---
+++ src/App copy.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import CustomWebcam from "./CustomWebcam.js"; // import it
+
+const App = () => {
+  return (
+    <div className="App">
+      <CustomWebcam />
+    </div>
+  );
+};
+
+
+export default App;
